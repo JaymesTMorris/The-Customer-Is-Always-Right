9af514ec92bc438e4b94fa1f73f9e86f8e79bee5
Jaymes T Morris <Jaymestmorris16@gmail.com>
2024-03-22 03:39:05 -0400

    Update draggable_object.gd
    - When dragging a draggable object, it creates a new instance of itself to remain behind only if doing so would instantiate one in the original slot for that object. (This gives the appearance of the original item never leaving the shelf. Instead, it looks like you took an object out of a pile of them.) [Temporary Fix]
    
    - Dropping a draggable object that is not on a slot deletes it.

--- Scripts/draggable_object.gd
+++ Scripts/draggable_object.gd
@@ -4,18 +4,21 @@ var draggable = false
 var is_inside_dropable = false
 var body_ref
 var offset: Vector2
-var initialPos: Vector2
+var initial_position: Vector2
+var is_cloneable = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	initial_position = position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if draggable:
 		if Input.is_action_just_pressed("click"):
-			initialPos = global_position
+			if is_cloneable:
+				_create_clone()
+				is_cloneable = false
 			offset = get_global_mouse_position() - global_position
 			global.is_dragging = true
 		if Input.is_action_pressed("click"):
@@ -28,7 +31,8 @@ func _process(delta):
 					queue_free()
 				tween.tween_property(self, "position", body_ref.position, 0.2).set_ease(Tween.EASE_OUT)
 			else:
-				tween.tween_property(self, "global_position", initialPos, 0.2).set_ease(Tween.EASE_OUT)
+				tween.tween_property(self, "global_position", initial_position, 0.2).set_ease(Tween.EASE_OUT)
+				tween.connect("finished", queue_free)
 
 func _on_area_2d_mouse_entered():
 	if not global.is_dragging:
@@ -53,3 +57,11 @@ func _on_area_2d_body_exited(body):
 	if body.is_in_group("dropable"):
 		is_inside_dropable = false
 		body.modulate = Color(Color.MEDIUM_PURPLE, 0.7)
+		
+func _create_clone():
+	var node = load("res://Prefabs/draggable_object.tscn")
+	var instance = node.instantiate()
+	instance.position = body_ref.position
+	instance.initial_position = body_ref.position
+	add_sibling(instance)
+	
